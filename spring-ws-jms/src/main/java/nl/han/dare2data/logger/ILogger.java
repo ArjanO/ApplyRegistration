10eec41ac42487e7f09ab326ba2f0dfb86901839
ArjanO <arjano@outlook.com>
2013-06-10 11:46:36 +0200

    Added ILogger interface.

---
+++ spring-ws-jms/src/main/java/nl/han/dare2data/logger/ILogger.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2013 HAN University of Applied Sciences
+ * Arjan Oortgiese
+ * Joëll Portier
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nl.han.dare2data.logger;
+
+/**
+ * Logger interface.
+ */
+public interface ILogger {
+    void trace(Object message);
+    void debug(Object message);
+    void info(Object message);
+    void warn(Object message);
+    void error(Object message);
+    void fatal(Object message);
+
+    void trace(Object message, Throwable t);
+    void debug(Object message, Throwable t);
+    void info(Object message, Throwable t);
+    void warn(Object message, Throwable t);
+    void error(Object message, Throwable t);
+    void fatal(Object message, Throwable t);
+}
