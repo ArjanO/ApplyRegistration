42875f542e388a909b8b8891b07a53d7393427cc
ArjanO <arjano@outlook.com>
2013-06-10 11:47:19 +0200

    Added Log4jWrapper.

---
+++ spring-ws-jms/src/main/java/nl/han/dare2data/logger/Log4jWrapper.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2013 HAN University of Applied Sciences
+ * Arjan Oortgiese
+ * Joëll Portier
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nl.han.dare2data.logger;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * Wrapper for Log4j.
+ */
+public class Log4jWrapper implements ILogger {
+    private static Logger logger = LogManager.getLogger(Log4jWrapper.class);
+
+    public void trace(Object message) {
+        logger.trace(message);
+    }
+
+    public void trace(Object message, Throwable t) {
+        logger.trace(message, t);
+    }
+
+    public void debug(Object message) {
+        logger.debug(message);
+    }
+
+    public void debug(Object message, Throwable t) {
+        logger.debug(message, t);
+    }
+
+    public void info(Object message) {
+        logger.info(message);
+    }
+
+    public void info(Object message, Throwable t) {
+        logger.info(message, t);
+    }
+
+    public void warn(Object message) {
+        logger.warn(message);
+    }
+
+    public void warn(Object message, Throwable t) {
+        logger.warn(message, t);
+    }
+
+    public void error(Object message) {
+        logger.error(message);
+    }
+
+    public void error(Object message, Throwable t) {
+        logger.error(message, t);
+    }
+
+    public void fatal(Object message) {
+        logger.fatal(message);
+    }
+
+    public void fatal(Object message, Throwable t) {
+        logger.fatal(message, t);
+    }
+}
