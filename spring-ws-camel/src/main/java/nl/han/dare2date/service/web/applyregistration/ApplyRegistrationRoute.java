492221f10b4e18e6d04f9862bf8fb1d3c67b44b7
ArjanO <arjano@outlook.com>
2013-06-24 10:06:01 +0200

    Added spring-ws-camel project skeleton.

---
+++ spring-ws-camel/src/main/java/nl/han/dare2date/service/web/applyregistration/ApplyRegistrationRoute.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2013 HAN University of Applied Sciences
+ * Arjan Oortgiese
+ * Joëll Portier
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nl.han.dare2date.service.web.applyregistration;
+
+import nl.han.dare2date.service.web.applyregistration.model.ApplyRegistrationRequest;
+import nl.han.dare2date.service.web.applyregistration.model.ApplyRegistrationResponse;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+
+public class ApplyRegistrationRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        JaxbDataFormat jaxb = new JaxbDataFormat(ApplyRegistrationRequest.class.getPackage().getName());
+
+        from("spring-ws:rootqname:{http://www.han.nl/schemas/messages}ApplyRegistrationRequest?endpointMapping=#applyRegistrationEndpointMapping")
+                .unmarshal(jaxb)
+                .process(new Echo()).
+                marshal(jaxb);
+    }
+
+    private static final class Echo implements Processor {
+        public void process(Exchange exchange) throws Exception {
+            ApplyRegistrationResponse registrationResponse = new ApplyRegistrationResponse();
+            registrationResponse.setRegistration(exchange.getIn().getBody(ApplyRegistrationRequest.class).getRegistration());
+            exchange.getOut().setBody(registrationResponse);
+        }
+    }
+}
